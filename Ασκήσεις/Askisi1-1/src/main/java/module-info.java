c5585f0bc5b7f13932de64821e771ece0084d8f1
panosale <panosalevr@hotmail.gr>
2024-10-16 00:51:06 +0300

    .

---
+++ Ασκήσεις/Askisi1-1/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module dipae_5thtemp.askisi11 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens dipae_5thtemp.askisi11 to javafx.fxml;
+    exports dipae_5thtemp.askisi11;
+}
