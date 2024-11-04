8da4831973582a3f02e6fafe7a41731089eb85ca
Panagiotis <62485609+panosale@users.noreply.github.com>
2024-11-04 10:07:06 +0200

    Update CreateTables.sql

--- TestPostgresWithNetbeans/src/CreateTables.sql
+++ TestPostgresWithNetbeans/src/CreateTables.sql
@@ -1,12 +1,12 @@
 DROP TABLE IF EXISTS t_Cities;
 DROP TABLE IF EXISTS t_Countries;
+DROP TABLE IF EXISTS t_RoomsCategories;
 
 CREATE TABLE IF NOT EXISTS t_Countries
 (
     f_CountryID SERIAL UNIQUE,
     f_CountryName character varying(30) PRIMARY KEY
 );
-
 INSERT INTO t_Countries (f_CountryName) VALUES ('GREECE');
 INSERT INTO t_Countries (f_CountryName) VALUES ('ITALY');
 INSERT INTO t_Countries (f_CountryName) VALUES ('FRANCE');
@@ -27,7 +27,18 @@ INSERT INTO t_Cities (f_CityName, f_CountryID) VALUES ('VENICE', 2);
 INSERT INTO t_Cities (f_CityName, f_CountryID) VALUES ('PARIS', 3);
 INSERT INTO t_Cities (f_CityName, f_CountryID) VALUES ('MADRID', 4);
 
+CREATE TABLE IF NOT EXISTS t_RoomsCategories (
+	f_CategoryID SERIAL UNIQUE,
+	f_CategoryName VARCHAR(50) PRIMARY KEY,
+	f_CategoryDescription TEXT
+);
+INSERT INTO t_RoomsCategories (f_CategoryName, f_CategoryDescription) VALUES ('ΚΑΝΟΝΙΚΟ ΔΩΜΑΤΙΟ', 'Κανονικό δωμάτιο.');
+INSERT INTO t_RoomsCategories (f_CategoryName, f_CategoryDescription) VALUES ('ΔΩΜΑΤΙΟ ΜΕ ΘΕΑ', 'Δωμάτιο με θέα στη θάλασσα.');
+INSERT INTO t_RoomsCategories (f_CategoryName) VALUES ('ΜΕΣΑΙΑ ΣΟΥΙΤΑ');
+INSERT INTO t_RoomsCategories (f_CategoryName, f_CategoryDescription) VALUES ('ΜΕΓΑΛΗ ΣΟΥΙΤΑ', 'Μεγάλη σουίτα γα βασιλιάδες.');
+
 -- TESTS
 -- SELECT t_Cities.f_CityID, t_Cities.f_CityName, t_Countries.f_CountryName AS f_CountryName FROM t_Cities JOIN t_Countries ON t_Cities.f_CountryID = t_Countries.f_CountryID
 -- select * from t_Cities;
 -- select * from t_Countries;
+-- select * from t_RoomsCategories;
