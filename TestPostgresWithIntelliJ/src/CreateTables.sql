5678d92e2d5bb78226885938818f48267716130a
Panagiotis <62485609+panosale@users.noreply.github.com>
2024-11-04 10:18:41 +0200

    Update CreateTables.sql

--- TestPostgresWithIntelliJ/src/CreateTables.sql
+++ TestPostgresWithIntelliJ/src/CreateTables.sql
@@ -1,13 +1,13 @@
 DROP TABLE IF EXISTS t_Cities;
 DROP TABLE IF EXISTS t_Countries;
 DROP TABLE IF EXISTS t_RoomsCategories;
+DROP TABLE IF EXISTS t_Services;
 
 CREATE TABLE IF NOT EXISTS t_Countries
 (
     f_CountryID SERIAL UNIQUE,
     f_CountryName VARCHAR(30) PRIMARY KEY
 );
-
 INSERT INTO t_Countries (f_CountryName) VALUES ('GREECE');
 INSERT INTO t_Countries (f_CountryName) VALUES ('ITALY');
 INSERT INTO t_Countries (f_CountryName) VALUES ('FRANCE');
@@ -33,14 +33,27 @@ CREATE TABLE IF NOT EXISTS t_RoomsCategories (
 	f_CategoryName VARCHAR(50) PRIMARY KEY,
 	f_CategoryDescription TEXT
 );
-
 INSERT INTO t_RoomsCategories (f_CategoryName, f_CategoryDescription) VALUES ('ΚΑΝΟΝΙΚΟ ΔΩΜΑΤΙΟ', 'Κανονικό δωμάτιο.');
 INSERT INTO t_RoomsCategories (f_CategoryName, f_CategoryDescription) VALUES ('ΔΩΜΑΤΙΟ ΜΕ ΘΕΑ', 'Δωμάτιο με θέα στη θάλασσα.');
 INSERT INTO t_RoomsCategories (f_CategoryName) VALUES ('ΜΕΣΑΙΑ ΣΟΥΙΤΑ');
 INSERT INTO t_RoomsCategories (f_CategoryName, f_CategoryDescription) VALUES ('ΜΕΓΑΛΗ ΣΟΥΙΤΑ', 'Μεγάλη σουίτα γα βασιλιάδες.');
 
+CREATE TABLE IF NOT EXISTS t_Services (
+	f_ServiceID SERIAL UNIQUE,
+	f_ServiceName VARCHAR(100) PRIMARY KEY,
+	f_ServicePrice FLOAT,
+	f_ServiceDescription TEXT
+);
+INSERT INTO t_Services (f_ServiceName, f_ServicePrice) VALUES ('ΔΙΑΝΥΚΤΕΡΕΥΣΗ ΕΝΗΛΙΚΟΥ', 80);
+INSERT INTO t_Services (f_ServiceName, f_ServicePrice, f_ServiceDescription) VALUES ('ΔΙΑΝΥΚΤΕΡΕΥΣΗ ΑΝΗΛΙΚΟΥ', 20, 'Διανυκτέρευση ανηλίκου κάτω των 18 ετών.');
+INSERT INTO t_Services (f_ServiceName, f_ServicePrice, f_ServiceDescription) VALUES ('ΠΡΩΙΝΟ', 8.50, 'Πρωινό στο εστιατόριο.');
+INSERT INTO t_Services (f_ServiceName, f_ServicePrice, f_ServiceDescription) VALUES ('ΠΡΩΙΝΟ ΔΩΜΑΤΙΟΥ', 15.50, 'Πρωινό στο δωμάτιο.');
+INSERT INTO t_Services (f_ServiceName, f_ServicePrice) VALUES ('ΣΠΑ', 20);
+
+
 -- TESTS
 -- SELECT t_Cities.f_CityID, t_Cities.f_CityName, t_Countries.f_CountryName AS f_CountryName FROM t_Cities JOIN t_Countries ON t_Cities.f_CountryID = t_Countries.f_CountryID
--- select * from t_Cities;
--- select * from t_Countries;
--- select * from t_RoomsCategories;
+--select * from t_Cities;
+--select * from t_Countries;
+--select * from t_RoomsCategories;
+--select * from t_Services;
